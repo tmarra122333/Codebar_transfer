054be0f722ac9f078b35e30f17b7934a1394a6f2
Tmarra122333 <Tmarra122333@gmail.com>
2022-02-03 13:26:02 -0500

    Student and Instructor created

--- codebar.py
+++ codebar.py
@@ -1,11 +1,25 @@
+from ast import BinOp
+
+
 class Member:
     def __init__(self, fullname):                           #Initializing the Class with __init__ (Always self first, then the attribute fullname)
         self.fullname = fullname                            #Use a dot method concept to create fullname 
         
     def member(self):
         print(f"Hello, my name is {self.fullname}")
+
 Thomas = Member("Thomas")
 Thomas.member()
+Jen = Member("Jen")
+Jen.member()
+class Student(Member):                                     #New student class is accessing member class through parameter () with super().
+    def __init__(self, fullname, reason):                  #Initializing class again new parameter of reason
+        super().__init__(fullname)                         #Essentialy unlocks the self.fullname from Member class
+        self.reason = reason                               #Dot method again to crease reason
 
-# class Student(Member):
-#     def __init__()
+class Instructor(Member):
+    def __init__(self, fullname, bio, skills):
+        super().__init__(fullname)
+        self.bio = bio
+        self.skills = skills
+
